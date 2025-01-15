e28efb8afd108dfa5dbeb6d85898181de9a061c9
Miguel Ángel Rodríguez <miguelangel.rodriguez.hernando@gmail.com>
2025-01-15 15:46:29 +0000

    Remove nextjs files

--- src/components/ConvertFeedback.tsx
+++
@@ -1,7 +0,0 @@
-interface ConvertFeedbackProps {
-  message: string;
-}
-
-export default function ConvertFeedback({ message }: ConvertFeedbackProps) {
-  return <p className="mb-5 text-center text-red-400">{message}</p>;
-}
